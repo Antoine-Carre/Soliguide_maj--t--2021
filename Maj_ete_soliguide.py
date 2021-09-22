0002c494dab5dcc72161de53fea4cae1ac34d726
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-09-22 14:19:14 +0200

    Update Maj_ete_soliguide.py

--- Maj_ete_soliguide.py
+++ Maj_ete_soliguide.py
@@ -184,6 +184,9 @@ if categorie == 'France':
 
     
     # Donnéés traitées pour construire graph 4
+    
+    res_france.rename(index={0: 'Soliguide', 1:'Acteurs'})
+    
     fig = px.pie(values=res_france.status, names=res_france.index, color_discrete_sequence=['palevioletred'])
     fig.update_traces(textinfo="percent+label")
     fig.update_layout(
