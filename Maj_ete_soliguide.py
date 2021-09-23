d8f835533a37455f1e4d4d30c37e46dadf846073
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-09-23 18:13:03 +0200

    Update Maj_ete_soliguide.py

--- Maj_ete_soliguide.py
+++ Maj_ete_soliguide.py
@@ -197,7 +197,8 @@ if categorie == 'France':
 
     st.markdown(html_string, unsafe_allow_html=True)
 
-    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>6 000 &emsp;&emsp;&emsp; 840 &emsp;&emsp;&emsp;&ensp; 287 960</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br></font>"
+    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>6 000 &emsp;&emsp;&emsp; 840 &emsp;&emsp;&emsp;&ensp; 287 960</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br>
+                                                                                                                                                                <br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&em(du 1<sup>er</sup> Juillet au 31 août)</font>"
 
     st.markdown(html_string, unsafe_allow_html=True)
 
@@ -298,7 +299,8 @@ if categorie == 'Ile-De-France':
 
     st.markdown(html_string, unsafe_allow_html=True)
 
-    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>4 351 &emsp;&emsp;&emsp; 421 &emsp;&emsp;&emsp;&ensp; 155 264</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br></font>"
+    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>4 351 &emsp;&emsp;&emsp; 421 &emsp;&emsp;&emsp;&ensp; 155 264</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br>
+                                                                                                                                                                <br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&em(du 1<sup>er</sup> Juillet au 31 août)</font>"
 
     st.markdown(html_string, unsafe_allow_html=True)
 
@@ -386,7 +388,8 @@ if categorie == 'Alpes-Maritimes (06)':
 
     st.markdown(html_string, unsafe_allow_html=True)
 
-    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>418 &emsp;&emsp;&emsp;  &emsp;&emsp;&emsp;&ensp; 4 120</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp;  &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br></font>"
+    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>418 &emsp;&emsp;&emsp;  &emsp;&emsp;&emsp;&ensp; 4 120</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp;  &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br>
+                                                                                                                                                                <br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&em(du 1<sup>er</sup> Juillet au 31 août)</font>"
 
     st.markdown(html_string, unsafe_allow_html=True)
 
