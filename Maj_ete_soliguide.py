63e286d15427936e76b383aa627f252ebf720ed0
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-09-24 09:58:08 +0200

    Update Maj_ete_soliguide.py

--- Maj_ete_soliguide.py
+++ Maj_ete_soliguide.py
@@ -197,7 +197,7 @@ if categorie == 'France':
 
     st.markdown(html_string, unsafe_allow_html=True)
 
-    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>6 000 &emsp;&emsp;&emsp; 840 &emsp;&emsp;&emsp;&ensp; 287 960</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;(du 1<sup>er</sup> Juillet au 31 août)</font>"
+    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>6 000 &emsp;&emsp;&emsp; 840 &emsp;&emsp;&emsp;&ensp; 287 960</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;(du 1<sup>er</sup> Juillet au 31 août)</font>"
                                                                                                                                                                 
 
     st.markdown(html_string, unsafe_allow_html=True)
