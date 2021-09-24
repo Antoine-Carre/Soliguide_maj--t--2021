90ebee4e2bbd930f06352572765f37008ed8807c
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-09-24 10:13:49 +0200

    Update Maj_ete_soliguide.py

--- Maj_ete_soliguide.py
+++ Maj_ete_soliguide.py
@@ -478,7 +478,7 @@ if categorie == 'Gironde (33)':
 
     st.markdown(html_string, unsafe_allow_html=True)
 
-    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>597 &emsp;&emsp;&emsp;&ensp; 268 &emsp;&emsp;&emsp;&ensp; 12 787</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br> <br></font>"
+    html_string = "<center><font color='#3E3A71' face='Helvetica' size='6'>597 &emsp;&emsp;&emsp;&ensp; 268 &emsp;&emsp;&emsp;&ensp; 12 787</font><br/><font size='2'>e-mails et relances envoyées &emsp;&emsp;&emsp;&emsp; appels effectués &emsp;&emsp;&emsp;&emsp; recherches réalisées sur Soliguide<br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;(du 1<sup>er</sup> Juillet au 31 août)</font>"
 
     st.markdown(html_string, unsafe_allow_html=True)
 
