0a1254ffc17276f722e5cd6659aa09298eb135b2
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-09-28 16:47:27 +0200

    Update Maj_ete_soliguide.py

--- Maj_ete_soliguide.py
+++ Maj_ete_soliguide.py
@@ -677,8 +677,7 @@ if categorie == 'Loire-Atlantique (44)':
     res_44.rename(index={0: 'Soliguide', 1:'Acteurs'}, inplace=True)
     
     fig = px.pie(res_44, values='status', names='index',
-             custom_data=['value'], labels={'lifeExp':'life expectancy','iso_num':'iso num'
-                                                      })
+             custom_data=['value'],)
     fig.update_traces(textposition='inside', textinfo='percent+label',\
                      hovertemplate = "Country:%{label}: <br>Population: %{customdata}")
     
